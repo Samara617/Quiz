c18fb0914d117c683d695086287f815e804160e3
Samara617 <gaulsamara@gmail.com>
2017-02-10 16:27:25 -0500

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,15 @@
+alert("Let's get this quiz started!");
+alert("Good luck!");
+document.getElementsByTagName('button');
+var button = document.querySelector('button');
+var quiz = document.getElementById('quiz');
+var question = (function () {
+    function question(value) {
+    }
+    return question;
+}());
+var answer = document.querySelector('text');
+var text = document.getElementsByName('q');
+button.onclick = function () {
+    alert('Nice Try');
+};
---
+++ quiz.ts
@@ -0,0 +1,22 @@
+alert ("Let's get this quiz started!");
+alert ("Good luck!");
+
+document.getElementsByTagName('button');
+let button = document.querySelector('button');
+let quiz = document.getElementById('quiz');
+
+class question {
+  answer:string
+
+  constructor (value:string) {
+
+
+  }
+}
+let answer = document.querySelector('text');
+let text = document.getElementsByName('q');
+
+
+button.onclick = function(){
+  alert('Nice Try');
+}
